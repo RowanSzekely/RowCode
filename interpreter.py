2bdcb846e2348cbc096ae1d0d181aeed6faaa151
Rowan Szekely <rowanszekely@uvic.ca>
2026-01-21 12:15:47 -0800

    Handle Null instead of using Python's None

--- interpreter.py
+++ interpreter.py
@@ -1,14 +1,11 @@
 from nodes import Node, NodeType
-from values import NumberVal
+from values import NumberVal, NullVal
 
 def evaluate(node):
     match node.type:
         # Will create seperate functions for evaluating program and future nodes
         case NodeType.PROGRAM:
-            last = None
-            for stmt in node.body:
-                last = evaluate(stmt)
-            return last
+            return eval_program(node)
         case NodeType.NUMERIC_LITERAL:
             return NumberVal(node.value)
         case NodeType.BINARY_EXPR:
@@ -16,7 +13,7 @@ def evaluate(node):
         case _:
             raise Exception(f"No evaluation rule for {node.type}")
 
-# Will move this
+# Will move these
 def eval_binary_expr(node):
 
     left = evaluate(node.left)
@@ -35,3 +32,12 @@ def eval_binary_expr(node):
         return NumberVal(left.value // right.value)
     if (node.operator == "%"):
         return NumberVal(left.value % right.value)
+    
+    raise Exception(f"Unknown operator {node.operator}")
+    
+def eval_program(node):
+
+    last = NullVal()
+    for stmt in node.body:
+        last = evaluate(stmt)
+    return last
--- values.py
+++ values.py
@@ -6,8 +6,15 @@ class NumberVal(RuntimeVal):
     def __init__(self, value: int):
         super().__init__("number")
         self.value = value
-        
+
     def __repr__(self):
         return f"NumberVal({self.value})"
 
-# Will add NullVal, BoolVal
+class NullVal(RuntimeVal):
+    def __init__(self):
+        super().__init__("null")
+        
+    def __repr__(self):
+        return "Null"
+
+# Will add BoolVal
