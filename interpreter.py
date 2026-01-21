3b3bb9d03249a478b15717a355fc8491ab43afda
Rowan Szekely <rowanszekely@uvic.ca>
2026-01-21 12:03:17 -0800

    Interpret binary expressions

--- interpreter.py
+++ interpreter.py
@@ -11,5 +11,27 @@ def evaluate(node):
             return last
         case NodeType.NUMERIC_LITERAL:
             return NumberVal(node.value)
+        case NodeType.BINARY_EXPR:
+            return eval_binary_expr(node)
         case _:
             raise Exception(f"No evaluation rule for {node.type}")
+
+# Will move this
+def eval_binary_expr(node):
+
+    left = evaluate(node.left)
+    right = evaluate(node.right)
+
+    if (left.type != "number" or right.type != "number"):
+        raise Exception("Only numbers supported when evaluating binary expressions")
+
+    if (node.operator == "+"):
+        return NumberVal(left.value + right.value)
+    if (node.operator == "-"):
+        return NumberVal(left.value - right.value)
+    if (node.operator == "*"):
+        return NumberVal(left.value * right.value)
+    if (node.operator == "/"):
+        return NumberVal(left.value // right.value)
+    if (node.operator == "%"):
+        return NumberVal(left.value % right.value)
